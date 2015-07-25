03a0e510caaaf16a4a64ad19d30859bf8e44af66
Geoffrey Donaldson <geoffreyd@gmail.com>
2015-07-25 22:08:57 +1000

    Add `and` Koan

--- tests/_ember-computed-test.js
+++ tests/_ember-computed-test.js
@@ -2,14 +2,15 @@ import { _ } from './helpers/blank';
 import Ember from 'ember';
 import { module, test, skip } from 'qunit';
 
-let count = 0, Animal, beast, cat;
 module('Koans | ? | Ember Computed', {});
 
+const Person = Ember.Object.extend({
+  name: 'My Name',
+  nick: Ember.computed.oneWay('name')
+});
+
 skip('computed.alias is an alias', function(assert) {
-  const thing = Ember.Object.create({
-    name: 'My Name',
-    nick: Ember.computed.alias('name')
-  });
+  const thing = Person.create();
 
   assert.equal(thing.get('nick'), _, "nick is the same as name");
   thing.set('nick', 'Other Name');
@@ -19,10 +20,7 @@ skip('computed.alias is an alias', function(assert) {
 });
 
 skip('computed.oneWay is a read-only alias', function(assert) {
-  const thing = Ember.Object.create({
-    name: 'My Name',
-    nick: Ember.computed.oneWay('name')
-  });
+  const thing = Person.create();
 
   assert.equal(thing.get('nick'), _);
   thing.set('nick', 'Other Name');
@@ -33,6 +31,19 @@ skip('computed.oneWay is a read-only alias', function(assert) {
   // This makes `oneWay` a good method for dynamic default values
 });
 
+skip('computed.and will logical `and` properties', function(assert) {
+  const trooper = Ember.Object.extend({
+    first: true,
+    second: false,
+    conOne: Ember.computed.and('first', 'second')
+  }).create();
+  assert.equal(trooper.get('conOne'), _, 'false and true');
+  trooper.set('second', true);
+  assert.equal(trooper.get('conOne'), _, 'true and true');
+  trooper.set('second', 'FINE!');
+  assert.equal(trooper.get('conOne'), _, 'Not just for booleans');
+});
+
 /*
-and any bool collect defaultTo deprecatingAlias empty equal filter filterBy filterProperty gt gte intersect lt lte map mapBy mapProperty match max min none not notEmpty or readOnly reads setDiff sort sum union uniq
+any bool collect defaultTo deprecatingAlias empty equal filter filterBy filterProperty gt gte intersect lt lte map mapBy mapProperty match max min none not notEmpty or readOnly reads setDiff sort sum union uniq
 */
